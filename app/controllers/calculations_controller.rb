9f6b6378393f718aff3270e9072efe7da0cd9f22
RamziAssaf <rassaf@chicagobooth.edu>
2015-10-23 16:32:37 -0500

    In class

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,4 +1,25 @@
 class CalculationsController < ApplicationController
+  def payment
+  	@year= params[:year].to_f
+  	@principal= params[:principal].to_f
+  	@rate= params[:rate].to_f
+  	@payment_result = (@principal * (1+@rate)**@year)
+  	render("payment.html.erb")
+  end
+
+  def random
+  	@low_bound = params[:low].to_i
+  	@high_bound = params[:high].to_i
+  	render("random.html.erb")
+  end
+
+  def sqrt
+  	# params = {"the_number" => "11238746464"}
+  	@number = params[:the_number].to_f
+  	@square_root = @number**0.5
+  	render("sqrt.html.erb")
+  end
+
   def instructions
     render("instructions.html.erb")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:the_number", { :controller => "calculations", :action => "sqrt" })
+  get("/random_number/:low/:high", { :controller => "calculations", :action => "random" })
+  get("/payment/:year/:principal/:rate", { :controller => "calculations", :action => "payment" })
 end
